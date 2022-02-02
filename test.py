6f21ebcc7f7970edb533ee831962aa0d00963269
Glutexo <glutexo@icloud.com>
2022-02-02 16:51:56 +0100

    Add describe tests

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from unittest import main
 from unittest import TestCase
 from unittest.mock import patch
 
+from code import describe
 from code import random_animal
 
 
@@ -32,6 +33,24 @@ class RandomAnimalTest(TestCase):
     def test_choice_result_is_returned(self, choice):
         animal = random_animal()
         assert animal is choice.return_value
+
+
+class DescribeTest(TestCase):
+    def test_cat(self):
+        description = describe("🐈")
+        assert description == "🐈 CAT"
+
+    @patch("code.unicode_name")
+    def test_unicode_name_is_used(self, unicode_name):
+        char = "🐈"
+        describe(char)
+        unicode_name.assert_called_once_with(char)
+
+    @patch("code.unicode_name")
+    def test_output_value(self, unicode_name):
+        char = "🐈"
+        description = describe(char)
+        assert description == f"{char} {unicode_name.return_value}"
 
 
 if __name__ == "__main__":
