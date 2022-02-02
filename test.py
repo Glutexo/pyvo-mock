ba56fa32037112e2d2b028a58fda9068d41b1da1
Glutexo <glutexo@icloud.com>
2022-02-02 16:39:24 +0100

    Add choice mocking tests

--- test.py
+++ test.py
@@ -1,15 +1,36 @@
 from unittest import main
 from unittest import TestCase
+from unittest.mock import patch
 
 from code import random_animal
 
 
 class RandomAnimalTest(TestCase):
-    def test_is_animal(self):
+    @staticmethod
+    def _read_animals():
         with open("animals.txt") as file:
-            animals = file.read()
+            return file.read()
+
+    def test_is_animal(self):
+        animals = self._read_animals()
         animal = random_animal()
         assert animal in animals
+
+    @patch("code.choice")
+    def test_choice_is_used(self, choice):
+        random_animal()
+        choice.assert_called_once()
+
+    @patch("code.choice")
+    def test_choice_is_used_with_animals(self, choice):
+        animals = self._read_animals()
+        random_animal()
+        choice.assert_called_once_with(animals)
+
+    @patch("code.choice")
+    def test_choice_result_is_returned(self, choice):
+        animal = random_animal()
+        assert animal is choice.return_value
 
 
 if __name__ == "__main__":
